96257548e0e5f25c0dbd715d063b4b99a54f9eab
Laercio Alberto A. de Ávila <laercio.aaavila@gmail.com>
2021-11-21 19:49:48 -0300

    Create Quarto.java

---
+++ atv-01/src/Quarto.java
@@ -0,0 +1,21 @@
+public class Quarto {
+
+    private int numero;
+    private int andar;
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public int getAndar() {
+        return andar;
+    }
+
+    public void setAndar(int andar) {
+        this.andar = andar;
+    }
+}
